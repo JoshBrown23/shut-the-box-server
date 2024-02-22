5e2e42911abec71a35a58b3c3111aa560764ee4e
Josh Brown <pokeboy99323@gmail.com>
2024-02-21 20:17:23 -0600

    Adding index.js and web.config files

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express'),
+app = express();
+
+//const cors = require("cors")
+
+var url = require('url');
+
+const port = process.env.PORT || 3000
+const majorVersion = 1
+const minorVersion = 3
+
+app.use(express.static(__dirname + '/static'))
+app.use(cors({ origin: '*' }))
+
+app.get('/about', (request, response) => {
+	console.log('Calling "/about" on the Node.js server.')
+	response.type('text/plain')
+	response.send('This is a website that utilizes server-side node.js to implement a dice rolling application.')
+})
+
+app.get('/generateNum1To6', (request, response) => {
+    console.log('Calling "/generateNum1To6" on the Node.js server.')
+    
+    let die = Math.floor(Math.random() * 6) + 1;
+
+    response.type('text/plain');
+    response.send(die);
+})
